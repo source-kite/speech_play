aad4171f5c3b61c19e427620e2f71dbf7672138e
HCL <hcl@lin760587583qing@163.com>
2018-09-22 17:37:43 +0800

    1. Use hash(md5) to reconize speech file. 2. Modify logic, Priority use of music.

--- scripts/speech_play_node.py
+++ scripts/speech_play_node.py
@@ -8,66 +8,94 @@ import time
 import filetype
 from std_msgs.msg import String
 import threading
+import hashlib
 
 voice_dict = {}
-voice_material_dir = '/tmp/voice_materials/'
+lock_of_voice_dict = threading.Lock()
+
+voice_material_dir = os.environ[ 'HOME' ] + '/Music/voice_materials/'
+
+print( voice_material_dir )
+
 
 def SpeechPlay_GetFiles( term_dir ):
-    term_list = os.listdir( term_dir )
+    term_list = os.listdir( voice_material_dir + 'music/' )
     file_list = []
 
     for term in term_list:
-        term_path = os.path.join( term_dir, term )
+        term_path = os.path.join( term_dir + 'music/', term )
         if( os.path.isfile( term_path ) ):
             file_list.append( term_path )
-
     return file_list
 
-def FileMonitor( ):
-    voice_file_list = SpeechPlay_GetFiles( voice_material_dir )
-    for file_path in voice_file_list:
-        voice_material_key = os.path.splitext( os.path.basename( file_path ) )[ 0 ] 
-        voice_dict[ voice_material_key ] = file_path
-
-    timer = threading.Timer( 1, FileMonitor )
-    timer.start()    
-
-    print( "file monitor" )
 
 def SpeechPlay_Callback( text ):
     global voice_publisher
     global tts_publisher
-    voice_material_key = text.data[ 0: 20 ] 
 
-    if( None != voice_dict.get( voice_material_key ) ):
-        if( os.path.isfile( voice_dict.get( voice_material_key ) ) ):
-            voice_publisher.publish( voice_dict.get( voice_material_key ) )
+    voice_material_key = text.data
+
+    lock_of_voice_dict.acquire()
+    local_dict = voice_dict
+    lock_of_voice_dict.release()
+
+    if( None != local_dict.get( voice_material_key ) ):
+        # Try the map way 
+        if( os.path.isfile( local_dict.get( voice_material_key ) ) ):
+            voice_publisher.publish( local_dict.get( voice_material_key ) )
             print( "Send file to voice play node.")
         else:
-            voice_dict.pop( voice_material_key )
+            local_dict.pop( voice_material_key )
             tts_publisher.publish( text )
             print( "Send text to tts node.")
     else:
-        tts_publisher.publish( text )
-        print( "Send text to tts node.")
-                
+        # Try the hash way
+        hash_md5 = hashlib.md5( text.data ).hexdigest()
+        speech_file_name = voice_material_dir + "speech/" + hash_md5 + ".wav"
+
+        if( os.path.isfile( speech_file_name ) ):
+            voice_publisher.publish( speech_file_name )
+            print( "Send file to voice play node.")
+        else:
+            tts_publisher.publish( text )
+            print( "Send text to tts node.")
             
-if __name__ == '__main__':
-    file_monitor = threading.Timer( 1, FileMonitor )
-    file_monitor.start()
-
-    try:
-        rospy.init_node( 'speech_play_node', anonymous=True )
-        rospy.Subscriber( 'SpeechPlay', String, SpeechPlay_Callback )
-        voice_publisher = rospy.Publisher( 'VoicePlay', String, queue_size = 1 )
-        tts_publisher = rospy.Publisher( 'tts', String, queue_size = 1 ) 
-
+def SpeechPlay_FileMonitor( ):
+    while( True ):
+        local_dict = {}
         voice_file_list = SpeechPlay_GetFiles( voice_material_dir )
 
         for file_path in voice_file_list:
             voice_material_key = os.path.splitext( os.path.basename( file_path ) )[ 0 ] 
-            voice_dict[ voice_material_key ] = file_path
+            local_dict[ voice_material_key ] = file_path
 
+        lock_of_voice_dict.acquire()
+        voice_dict = local_dict
+        lock_of_voice_dict.release()
+
+        time.sleep( 3 )
+
+if __name__ == '__main__':
+
+    if( False == os.path.exists( voice_material_dir ) ):
+        os.mkdir( voice_material_dir )
+
+    if( False == os.path.exists( voice_material_dir + 'music/' ) ):
+        os.mkdir( voice_material_dir + 'music/' )
+    
+    if( False == os.path.exists( voice_material_dir + 'speech/' ) ):
+        os.mkdir( voice_material_dir + 'speech/' )
+
+    file_monitor = threading.Thread( target = SpeechPlay_FileMonitor, name = "FileMonitor" )
+    file_monitor.setDaemon( True )
+    file_monitor.start()
+
+    rospy.init_node( 'speech_play_node', anonymous=True )
+    rospy.Subscriber( 'SpeechPlay', String, SpeechPlay_Callback )
+    voice_publisher = rospy.Publisher( 'VoicePlay', String, queue_size = 1 )
+    tts_publisher = rospy.Publisher( 'xfyun_tts', String, queue_size = 1 ) 
+
+    try:
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
